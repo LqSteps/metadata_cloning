39a5bcb49d6c1c6706cb7f8fb88393feca69b1d9
LqSteps <lqsteps@proton.me>
2026-09-20 22:42:44 +0200

    Adicionar tutorial e prompt para aceitar ou cancelar o script

--- scripts/metadata_cloning.sh
+++ scripts/metadata_cloning.sh
@@ -1,56 +1,78 @@
 #!/usr/bin/env bash
 
-if ! command -v exiftool &>/dev/null; then
-    if command -v apt-get &>/dev/null; then
-        apt-get update && apt-get install -y libimage-exiftool-perl
-    elif command -v pacman &>/dev/null; then
-        pacman -Sy --noconfirm perl-image-exiftool
-    elif command -v dnf &>/dev/null; then
-        dnf install -y perl-Image-ExifTool
-    elif command -v zypper &>/dev/null; then
-        zypper --non-interactive install perl-Image-ExifTool
-    elif command -v apk &>/dev/null; then
-        apk add exiftool
-    else
-        echo "[ERRO] Package manager não suportado." >&2
-        exit 1
-    fi
+echo -e "[TUTORIAL]\n\n \
+1. Adicione o PDF original em Desktop/metadata_cloning/PDFs/Originais.\n \
+2. Adicione o editado em Desktop/metadata_cloning/PDFs/Editados.\n \
+3. Renomeie o editado para exatamente o mesmo nome do documento real.\n"
+
+echo -e "[AVISO] Este script não funcionará corretamente se o nome do pdf original e do editado não forem idêntios.\n"
+
+echo "Iniciar script? [s/n]"
+
+read -r argX
+
+if [[ ! "$argX" =~ ^[sS]$ ]]; then
+
+	echo "[AVISO] Script encerrado pelo usuário."
+	exit 1
+
+else
+
+	echo "[AVISO] Executando o script"
+
+	if ! command -v exiftool &>/dev/null; then
+	    if command -v apt-get &>/dev/null; then
+		apt-get update && apt-get install -y libimage-exiftool-perl
+	    elif command -v pacman &>/dev/null; then
+		pacman -Sy --noconfirm perl-image-exiftool
+	    elif command -v dnf &>/dev/null; then
+		dnf install -y perl-Image-ExifTool
+	    elif command -v zypper &>/dev/null; then
+		zypper --non-interactive install perl-Image-ExifTool
+	    elif command -v apk &>/dev/null; then
+		apk add exiftool
+	    else
+		echo "[ERRO] Package manager não suportado." >&2
+		exit 1
+	    fi
+	fi
+
+	readonly ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+	mkdir -p "$ROOT_DIR/PDFs"
+	mkdir -p "$ROOT_DIR/PDFs/Originais"
+	mkdir -p "$ROOT_DIR/PDFs/Editados"
+
+	mapfile -d "" files < <(
+		find "$ROOT_DIR/PDFs/Originais/" \
+		-type f \
+		-iname "*.pdf" \
+		-print0
+	)
+
+	for i in "${files[@]}"; do
+		base_name="$(basename "$i")"
+		output="$ROOT_DIR/PDFs/Editados/$base_name"
+
+		if [ ! -f "$output" ]; then
+			
+			echo "[ERRO] $output não existe."
+			continue
+		fi
+
+		if exiftool -TagsFromFile "$i" \
+		-all:all -P \
+		-overwrite_original "$output" &>/dev/null; then
+
+			echo "[SUCESSO] $output com metadados idênticos ao original."
+
+		else
+			echo "[ERRO] $output não recebeu metadados do original."
+
+		fi
+
+
+		touch -r "$i" "$output"
+	done
+
 fi
-
-readonly ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-
-mkdir -p "$ROOT_DIR/PDFs"
-mkdir -p "$ROOT_DIR/PDFs/Originais"
-mkdir -p "$ROOT_DIR/PDFs/Editados"
-
-mapfile -d "" files < <(
-	find "$ROOT_DIR/PDFs/Originais/" \
-	-type f \
-	-iname "*.pdf" \
-	-print0
-)
-
-for i in "${files[@]}"; do
-	base_name="$(basename "$i")"
-	output="$ROOT_DIR/PDFs/Editados/$base_name"
-
-	if [ ! -f "$output" ]; then
-		
-		echo "[ERRO] $output não existe."
-		continue
-	fi
-
-	if exiftool -TagsFromFile "$i" \
-	-all:all -P \
-	-overwrite_original "$output" &>/dev/null; then
-
-		echo "[SUCESSO] $output com metadados idênticos ao original."
-
-	else
-		echo "[ERRO] $output não recebeu metadados do original."
-
-	fi
-
-
-	touch -r "$i" "$output"
-done
